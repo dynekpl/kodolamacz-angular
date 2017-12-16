9ac168b3e3c97940b557aa832e5fe5d529836be8
dynekpl <dynek82@gmail.com>
2017-12-16 12:59:49 +0100

    First component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  books = [
+  {
+    id: '1',
+    title: 'Angular in action',
+    authors: 'Jan Kowalski',
+    category : "#00ff00",
+    bestseller : true
+  },
+  {
+    id: '2',
+    title: 'React in action',
+    authors: 'Marek Nowak',
+    category : "#000000",
+    bestseller : false
+  }
+  ];
+
+  selectedBook = null;
+  editedBook = null;
+
+  select(book){
+    this.selectedBook = book;
+    this.editedBook = Object.assign({}, book);
+  }
+
+  save(){
+    Object.assign(this.selectedBook, this.editedBook;
+    this.reset();
+  }
+
+  reset(){
+    this.selectedBook = null;
+    this.editedBook = null;
+  }
 }
